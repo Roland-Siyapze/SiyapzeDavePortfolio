a23dd01e1d78c6823d799088599c22c1a0609887
Siyapze Dave Roland <rsiyapze@gmail.com>
2025-03-10 14:53:28 +0100

    portfolio

---
+++ README.md
@@ -0,0 +1,38 @@
+# Portfolio - [Siyapze_Dave_Roland]
+
+Welcome to my portfolio! This repository contains the source code and assets for my personal portfolio website.
+
+## About Me
+
+[Insert your short introduction overview here. Example: "I'm a versatile Full-Stack Developer with a passion for building dynamic and optimized web applications. Proficient in C# .NET, PHP Laravel, and modern frontend frameworks like Vue.js and Razor, I prioritize creating seamless user experiences and delivering high-performance solutions."]
+
+## Technologies Used
+
+- **Frontend:** [React, Tailwind, ThreeJs]
+
+## Portfolio Sections
+
+- **About:** A brief overview of my skills and experience.
+- **Services:** A list of the services I offer.
+- **Experience:** A timeline of my professional experience.
+- **Projects:** A showcase of my projects with descriptions and links.
+- **Contact:** Contact information and a form to get in touch.
+
+## Getting Started
+
+1.  **Clone the repository:**
+    ```bash
+    git clone [repository URL]
+    ```
+2.  **Navigate to the project directory:**
+    ```bash
+    cd [project directory]
+    ```
+3.  **Install dependencies (if applicable):**
+    ```bash
+    npm install  # or yarn install, composer install
+    ```
+
+## Contact
+
+- **Email:** rsiyapze@gmail.com
---
+++ src/constants/index.js
@@ -0,0 +1,294 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  iziway,
+  carrent,
+  jobit,
+  tripguide,
+  webverseagency,
+  campusconnect,
+  skilltestapp,
+  threejs,
+  laravel,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "React Native Developer",
+    icon: mobile,
+  },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+  {
+    title: "API Development",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  // {
+  //   name: "TypeScript",
+  //   icon: typescript,
+  // },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Laravel",
+    icon: laravel,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  // {
+  //   name: "figma",
+  //   icon: figma,
+  // },
+  // {
+  //   name: "docker",
+  //   icon: docker,
+  // },
+];
+
+const experiences = [
+  // {
+  //   title: "React.js Developer",
+  //   company_name: "Starbucks",
+  //   icon: starbucks,
+  //   iconBg: "#383E56",
+  //   date: "March 2020 - April 2021",
+  //   points: [
+  //     "Developing and maintaining web applications using React.js and other related technologies.",
+  //     "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+  //     "Implementing responsive design and ensuring cross-browser compatibility.",
+  //     "Participating in code reviews and providing constructive feedback to other developers.",
+  //   ],
+  // },
+  {
+    title: "Frontend Development",
+    company_name: "IZIWAY, Douala",
+    icon: iziway,
+    iconBg: "#E6DEDD",
+    date: "June 2023 - October 2023",
+    points: [
+      "Designed and integrated dynamic user interfaces using Razor, adhering to UI/UX designs.",
+      "Developed reusable components and implemented features like infinite scrolling.",
+      "Resolved frontend bugs and optimized web page performance.",
+      "Collaborated with the backend team to ensure seamless API integration.",
+    ],
+  },
+  {
+    title: "Full-Stack Development",
+    company_name: "IZIWAY, Douala",
+    icon: iziway,
+    iconBg: "#383E56",
+    date: "June 2024 - November 2024",
+    points: [
+      "Developed features in C# .NET, enhancing e-commerce site performance and user experience.",
+      "Optimized the Seller Center platform with PHP Laravel for efficient product management.",
+      "Designed REST APIs to synchronize data between the marketplace and Seller Center.",
+      "Resolved critical bugs and optimized SQL queries on SQL Server.",
+      "Implemented product approval and rejection functionalities for improved quality control.",
+    ],
+  },
+  // {
+  //   title: "React Native Developer",
+  //   company_name: "Tesla",
+  //   icon: tesla,
+  //   iconBg: "#E6DEDD",
+  //   date: "Jan 2021 - Feb 2022",
+  //   points: [
+  //     "Developing and maintaining web applications using React.js and other related technologies.",
+  //     "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+  //     "Implementing responsive design and ensuring cross-browser compatibility.",
+  //     "Participating in code reviews and providing constructive feedback to other developers.",
+  //   ],
+  // },
+  // {
+  //   title: "Web Developer",
+  //   company_name: "Shopify",
+  //   icon: shopify,
+  //   iconBg: "#383E56",
+  //   date: "Jan 2022 - Jan 2023",
+  //   points: [
+  //     "Developing and maintaining web applications using React.js and other related technologies.",
+  //     "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+  //     "Implementing responsive design and ensuring cross-browser compatibility.",
+  //     "Participating in code reviews and providing constructive feedback to other developers.",
+  //   ],
+  // },
+  // {
+  //   title: "Full stack Developer",
+  //   company_name: "Meta",
+  //   icon: meta,
+  //   iconBg: "#E6DEDD",
+  //   date: "Jan 2023 - Present",
+  //   points: [
+  //     "Developing and maintaining web applications using React.js and other related technologies.",
+  //     "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+  //     "Implementing responsive design and ensuring cross-browser compatibility.",
+  //     "Participating in code reviews and providing constructive feedback to other developers.",
+  //   ],
+  // },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "Webverse Digital Agency",
+    description:
+      "A digital agency website offering app development, web design, and graphic design, with client testimonials and project examples.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mysql",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: webverseagency,
+    source_code_link: "https://github.com/Roland-Siyapze/WebVerse-Digital",
+  },
+  {
+    name: "SkillTest App",
+    description:
+      "Innovative coding assessment platform with AI-driven analysis, offering students and companies a more accurate evaluation of programming skills.",
+    tags: [
+      {
+        name: "laravel",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "vuejs",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: skilltestapp,
+    source_code_link: "https://github.com/Roland-Siyapze/SKILLTEST_APP",
+  },
+  {
+    name: "Campus Connect",
+    description:
+      "Web app for a comprehensive digital campus experience with networking, event calendar, chat, and campus store.",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "supabase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: campusconnect,
+    source_code_link:
+      "https://github.com/Roland-Siyapze/campus-connectivity-nexus",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
