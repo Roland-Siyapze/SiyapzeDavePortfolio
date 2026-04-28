ef39004e15bd6e03cf7501928186db3b64f3f230
Siyapze Dave Roland <rsiyapze@gmail.com>
2026-04-28 13:17:03 +0100

    style(styles): adjust horizontal padding for extra small screens
    Update paddingX and padding values to use a specific pixel value for
    extra small devices to improve layout spacing.

--- src/styles.js
+++ src/styles.js
@@ -1,7 +1,7 @@
 const styles = {
-  paddingX: 'xs:px-8 px-4 sm:px-16',
+  paddingX: 'xs:px-[0.7] px-4 sm:px-16',
   paddingY: 'xs:py-8 py-6 sm:py-16',
-  padding: 'xs:px-8 px-4 xs:py-8 py-6 sm:px-16 sm:py-16',
+  padding: 'xs:px-[0.7] px-4 xs:py-8 py-6 sm:px-16 sm:py-16',
 
   heroHeadText:
     'font-black xs:text-[48px] text-[36px] sm:text-[60px] md:text-[80px] xs:leading-[60px] sm:leading-[80px] md:leading-[98px] mt-2',
@@ -10,7 +10,8 @@ const styles = {
 
   sectionHeadText:
     'font-black xs:text-[36px] text-[28px] sm:text-[50px] md:text-[60px]',
-  sectionSubText: 'xs:text-[16px] text-[12px] sm:text-[18px] uppercase tracking-wider',
+  sectionSubText:
+    'xs:text-[16px] text-[12px] sm:text-[18px] uppercase tracking-wider',
 };
 
 export { styles };
